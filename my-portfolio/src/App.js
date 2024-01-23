8582cda3ac738e82bab5a03ef7a8e08062ae7570
Weihao Zhou <ouihow@weihaodeair.home>
2024-01-23 22:28:14 +0100

    Initial commit

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,25 +1,13 @@
-/*
- * @Author: Weihao Zhou 周炜昊
- * @Date: 2024-01-23 22:06:32
- * @LastEditTime: 2024-01-23 22:09:33
- * @LastEditors: Weihao Zhou 周炜昊
- * @FilePath: /my-portfolio/src/App.js
- * @Description: 
- * 北京数城未来科技有限公司
- */
 // src/App.js
 
 import React from 'react';
 import './App.css';
+import Profile from './components/Profile';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Your Name</h1>
-        <p>Web Developer</p>
-        {/* 添加其他信息 */}
-      </header>
+      <Profile />
     </div>
   );
 }
---
+++ my-portfolio/src/components/Profile.js
@@ -0,0 +1,16 @@
+
+// src/components/Profile.js
+
+import React from 'react';
+import '../styles/Profile.css';
+const Profile = () => {
+  return (
+    <div>
+      <h1>Your Name</h1>
+      <p>Web Developer</p>
+      {/* 添加其他信息 */}
+    </div>
+  );
+}
+
+export default Profile;
