717720ac5e9c3099216984060d3c4783fb85231c
Weihao Zhou <ouihow@weihaodeair.home>
2024-01-24 10:51:55 +0100

    add component

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import './App.css';
-import Profile from './components/Profile';
+import Header from './components/Header';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div className="App">
-      <Profile />
+      <Header />
+      <Skills />
     </div>
   );
 }
---
+++ my-portfolio/src/components/Skills.js
@@ -0,0 +1,20 @@
+
+// src/components/Skills.js
+
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <section>
+      <h2>Skills</h2>
+      <ul>
+        <li>React</li>
+        <li>Vue.js</li>
+        <li>Node.js</li>
+        {/* 添加其他技能 */}
+      </ul>
+    </section>
+  );
+}
+
+export default Skills;
